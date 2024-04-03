1142ced1032dd48931aa43c7ebc9593d45ffd2f3
sergievpasad <ser.90mast@mail.ru>
2024-04-03 21:49:50 +0300

    Update Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,8 +6,35 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            string key;
+            int a,b;
             Console.WriteLine("Главное меню:\nСложение(+)\nВычитание(-)\nДеление(/)\t\t  Символы в скобках\nУмножение(*)\t   подразумевают синтаксик который\nСтепень(sqr)\t\tдолжен использоваться\nКорень(sqrt)\nФакториал(!)\nМодуль(mod)\nСинус(sin)\nКосинус(cos)\nТангенс(tg)\nКотангенс(ctg)\nДонаты(don)\nВозврат в главное меню(gob)\nВыход из консоли(end)");
-            Console.ReadKey();  
+            key = Console.ReadLine();
+            while (key!="end")
+            {
+                Console.Clear();
+                switch (key)
+                {
+                    case "+":
+                        Console.Write("Введите первое число: ");
+                        a = Convert.ToInt32(Console.ReadLine());
+                        Console.Write("Введите второе число: ");
+                        b = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine($"Результат: {a+b}") ;
+                        break;
+                    case "*":
+                        Console.Write("Введите первое число: ");
+                        a = Convert.ToInt32(Console.ReadLine());
+                        Console.Write("Введите второе число: ");
+                        b = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine($"Результат: {a * b}");
+                        break;
+                        
+                   
+
+                }
+                key = Console.ReadLine();
+            }
         }
     }
 }
